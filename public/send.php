0c088d04ccac979195b2eee0e87f522089755317
xxxtentacali <xxxtentacionxxxtentacionxxx227@gmail.com>
2025-01-20 06:21:58 +0330

    Update send.php

--- public/send.php
+++ public/send.php
@@ -2,7 +2,6 @@
 
 $ip = "localhost:8000";
 
-
 $manager = $_POST['handler'];
 
 if ($manager == 'tx'){
@@ -10,61 +9,66 @@ if ($manager == 'tx'){
     $chain = $_POST['chain'];
     $type = $_POST['type'];
 
+    // تعیین URL بر اساس نوع و شبکه
     if ($type == "coin"){
-        $url = "http://$ip/tx/?address=$address&chain=$chain";
+        if ($chain == "1") {
+            // اتریوم
+            $url = "http://$ip/tx/?address=$address&chain=$chain";
+        } elseif ($chain == "56") {
+            // BSC
+            $url = "http://$ip/tx/?address=$address&chain=$chain";
+        } else {
+            // شبکه ناشناخته
+            echo json_encode(["error" => "Unsupported network"]);
+            exit;
+        }
 
         $request = curl_init();
-        curl_setopt($request,CURLOPT_URL,$url);
-        curl_setopt($request,CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($request, CURLOPT_URL, $url);
+        curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
         $output = curl_exec($request);
-    
+
         echo $output;
-    
-    }else{
+
+    } else {
+        // برای توکن‌ها
         $contract = $_POST['contract'];
-        $url = "http://$ip/txt/?address=$address&chain=$chain&contract=$contract";
+
+        if ($chain == "1") {
+            // اتریوم
+            $url = "http://$ip/txt/?address=$address&chain=$chain&contract=$contract";
+        } elseif ($chain == "56") {
+            // BSC
+            $url = "http://$ip/txt/?address=$address&chain=$chain&contract=$contract";
+        } else {
+            // شبکه ناشناخته
+            echo json_encode(["error" => "Unsupported network"]);
+            exit;
+        }
 
         $request = curl_init();
-        curl_setopt($request,CURLOPT_URL,$url);
-        curl_setopt($request,CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($request, CURLOPT_URL, $url);
+        curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
         $output = curl_exec($request);
-    
+
         echo $output;
-    
     }
-
-
-
-
-}else if ($manager == "sign"){
+} elseif ($manager == "sign") {
     $signature = $_POST['signature'];
     $type = $_POST['type'];
 
-    if ($type == "coin"){
+    // تعیین URL برای امضا بر اساس نوع
+    if ($type == "coin") {
         $url = "http://$ip/sr/?signature=$signature";
-
-        $request = curl_init();
-        curl_setopt($request,CURLOPT_URL,$url);
-        curl_setopt($request,CURLOPT_RETURNTRANSFER,true);
-        $output = curl_exec($request);
-    
-        echo $output;
-    }else{
+    } else {
         $url = "http://$ip/srt/?signature=$signature";
-
-        $request = curl_init();
-        curl_setopt($request,CURLOPT_URL,$url);
-        curl_setopt($request,CURLOPT_RETURNTRANSFER,true);
-        $output = curl_exec($request);
-    
-        echo $output;
     }
 
+    $request = curl_init();
+    curl_setopt($request, CURLOPT_URL, $url);
+    curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+    $output = curl_exec($request);
 
-
+    echo $output;
 }
-
-
-
-
 ?>
