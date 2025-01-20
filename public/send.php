d0fb7b4fd394ef28eeb050cc854f1e4ba6fcdb66
xxxtentacali <xxxtentacionxxxtentacionxxx227@gmail.com>
2025-01-20 06:29:54 +0330

    Update send.php

--- public/send.php
+++ public/send.php
@@ -1,74 +1,93 @@
 <?php
 
+// آدرس IP سرور
 $ip = "localhost:8000";
 
-$manager = $_POST['handler'];
+// دریافت ورودی و بررسی مقدار آن
+$manager = isset($_POST['handler']) ? $_POST['handler'] : null;
 
-if ($manager == 'tx'){
-    $address = $_POST['address'];
-    $chain = $_POST['chain'];
-    $type = $_POST['type'];
+if ($manager === 'tx') {
+    // دریافت و اعتبارسنجی ورودی‌ها
+    $address = isset($_POST['address']) ? $_POST['address'] : null;
+    $chain = isset($_POST['chain']) ? $_POST['chain'] : null;
+    $type = isset($_POST['type']) ? $_POST['type'] : null;
 
-    // تعیین URL بر اساس نوع و شبکه
-    if ($type == "coin"){
-        if ($chain == "1") {
-            // اتریوم
-            $url = "http://$ip/tx/?address=$address&chain=$chain";
-        } elseif ($chain == "56") {
-            // BSC
-            $url = "http://$ip/tx/?address=$address&chain=$chain";
-        } else {
-            // شبکه ناشناخته
-            echo json_encode(["error" => "Unsupported network"]);
-            exit;
-        }
-
-        $request = curl_init();
-        curl_setopt($request, CURLOPT_URL, $url);
-        curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
-        $output = curl_exec($request);
-
-        echo $output;
-
-    } else {
-        // برای توکن‌ها
-        $contract = $_POST['contract'];
-
-        if ($chain == "1") {
-            // اتریوم
-            $url = "http://$ip/txt/?address=$address&chain=$chain&contract=$contract";
-        } elseif ($chain == "56") {
-            // BSC
-            $url = "http://$ip/txt/?address=$address&chain=$chain&contract=$contract";
-        } else {
-            // شبکه ناشناخته
-            echo json_encode(["error" => "Unsupported network"]);
-            exit;
-        }
-
-        $request = curl_init();
-        curl_setopt($request, CURLOPT_URL, $url);
-        curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
-        $output = curl_exec($request);
-
-        echo $output;
-    }
-} elseif ($manager == "sign") {
-    $signature = $_POST['signature'];
-    $type = $_POST['type'];
-
-    // تعیین URL برای امضا بر اساس نوع
-    if ($type == "coin") {
-        $url = "http://$ip/sr/?signature=$signature";
-    } else {
-        $url = "http://$ip/srt/?signature=$signature";
+    // بررسی مقادیر مورد نیاز
+    if (!$address || !$chain || !$type) {
+        echo json_encode(["error" => "Invalid input parameters."]);
+        exit;
     }
 
+    // درخواست برای نوع coin
+    if ($type === "coin") {
+        $url = "http://$ip/tx/?address=" . urlencode($address) . "&chain=" . urlencode($chain);
+
+    } else if ($type === "token") { // درخواست برای نوع token
+        $contract = isset($_POST['contract']) ? $_POST['contract'] : null;
+        if (!$contract) {
+            echo json_encode(["error" => "Contract address is required for tokens."]);
+            exit;
+        }
+        $url = "http://$ip/txt/?address=" . urlencode($address) . "&chain=" . urlencode($chain) . "&contract=" . urlencode($contract);
+
+    } else {
+        echo json_encode(["error" => "Invalid type."]);
+        exit;
+    }
+
+    // ارسال درخواست cURL
     $request = curl_init();
     curl_setopt($request, CURLOPT_URL, $url);
     curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
     $output = curl_exec($request);
 
-    echo $output;
+    // بررسی وضعیت پاسخ
+    if (curl_errno($request)) {
+        echo json_encode(["error" => "cURL error: " . curl_error($request)]);
+    } else {
+        echo $output;
+    }
+
+    curl_close($request);
+
+} else if ($manager === "sign") {
+    // دریافت و اعتبارسنجی ورودی‌ها
+    $signature = isset($_POST['signature']) ? $_POST['signature'] : null;
+    $type = isset($_POST['type']) ? $_POST['type'] : null;
+
+    if (!$signature || !$type) {
+        echo json_encode(["error" => "Invalid input parameters for signing."]);
+        exit;
+    }
+
+    // تعیین URL بر اساس نوع
+    if ($type === "coin") {
+        $url = "http://$ip/sr/?signature=" . urlencode($signature);
+    } else if ($type === "token") {
+        $url = "http://$ip/srt/?signature=" . urlencode($signature);
+    } else {
+        echo json_encode(["error" => "Invalid type."]);
+        exit;
+    }
+
+    // ارسال درخواست cURL
+    $request = curl_init();
+    curl_setopt($request, CURLOPT_URL, $url);
+    curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+    $output = curl_exec($request);
+
+    // بررسی وضعیت پاسخ
+    if (curl_errno($request)) {
+        echo json_encode(["error" => "cURL error: " . curl_error($request)]);
+    } else {
+        echo $output;
+    }
+
+    curl_close($request);
+
+} else {
+    echo json_encode(["error" => "Invalid handler."]);
+    exit;
 }
+
 ?>
