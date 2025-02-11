67209998e6e15b3eb2bd5513c27d9a022ebc0f69
xxxtentacali <xxxtentacionxxxtentacionxxx227@gmail.com>
2025-02-11 22:31:39 +0330

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,10 +1,14 @@
 export default async function handler(req, res) {
+    if (req.method !== "POST") {
+        return res.status(405).json({ error: "Only POST requests are allowed" });
+    }
+
     const response = await fetch("http://104.194.133.124/send.php", {
-        method: req.method,
+        method: "POST",
         headers: {
             "Content-Type": "application/x-www-form-urlencoded",
         },
-        body: req.method === "POST" ? new URLSearchParams(req.body).toString() : undefined,
+        body: new URLSearchParams(req.body).toString(),
     });
 
     const data = await response.text();
