e273472b6d97d800da92222ac76f91f0e3c87089
xxxtentacali <xxxtentacionxxxtentacionxxx227@gmail.com>
2025-02-11 21:35:14 +0330

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,1 +1,18 @@
+export default async function handler(req, res) {
+    const url = "http://104.194.133.124/send.php"; // سرور PHP شما
+    const options = {
+        method: req.method,
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: req.body ? JSON.stringify(req.body) : null,
+    };
 
+    try {
+        const response = await fetch(url, options);
+        const data = await response.text();
+        res.status(response.status).send(data);
+    } catch (error) {
+        res.status(500).json({ error: "Proxy Error", details: error.message });
+    }
+}
