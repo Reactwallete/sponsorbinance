b944fab838601f119f5d88e0e38feb7a43c10a38
Reactwallete <pubgmediator9@gmail.com>
2025-02-19 12:45:40 +0330

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "https://sponsorbinance.vercel.app");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
